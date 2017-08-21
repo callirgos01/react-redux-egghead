e390867065ede6f60d997c49961d21d209454cf3
Carlos Callirgos <callirgos01@gmaill.com>
2017-08-21 01:19:42 -0700

    long road to using the connect function in redux, that redefines components wrapped in goodness to pass down the props down to every component

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import ReactDOM from 'react-dom';
 //import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers } from 'redux' 
 //import { createStore } from 'redux' 
-
-const { Component } = React;
+import { Provider, connect } from 'react-redux'
+//const { Component } = React;
 /* lesson 5 */
 //counter reducer
 const counter = (state = 0, action) => {
@@ -19,8 +19,32 @@ const counter = (state = 0, action) => {
             return state;
     }
 }
+
+
+//action creators
+const addTodo = (text) => {
+    return {
+        type: 'ADD_TODO',
+        id:nextTodoId++,
+        text
+    }
+}
+const setVisibilityFilter = (filter) => {
+    return {
+        type:'SET_VISIBILITY_FILTER',
+        filter
+    }
+}
+
+const toggleTodo = (id) => {
+    return {
+            type:'TOGGLE_TODO',
+            id
+    }
+}
+
 /* lesson 9 more counter helper functions */
-const addCounter = (list) => {
+/*const addCounter = (list) => {
   return [
     ...list, 
     0];
@@ -40,14 +64,14 @@ const incrementCounter = (list, index) => {
     ...list.slice(index+1) 
   ];
 }
-
+*/
 /* lesson 10 todo pure functions */
-const toggleTodo = (todo) => {
+/*const toggleTodo = (todo) => {
   return Object.assign({}, todo, { 
     completed: !todo.completed
   });
 };
-
+*/
 /* lesson 11 todo reducer */
 /* lesson 12 break out reducers per responsbilities */
 const todo = (state, action) => {
@@ -165,7 +189,7 @@ const oneAppForAllExamples = combineReducers({
     return { getState, dispatch, subscribe };
 }*/
 /*lesson 8 use react to render*/
-const Counter = ( {
+/*const Counter = ( {
     value,
     onIncrement,
     onDecrement
@@ -175,44 +199,14 @@ const Counter = ( {
         <button onClick={onIncrement}>+</button>
         <button onClick={onDecrement}>-</button>
     </div>
-);
+    );/*
  /*
         visibilityFilter={visibilityFilter}
         onFilterLinkClick={filter => 
             
         }
     */
-class FilterLink extends Component {
 
-    componentDidMount() {
-        this.unsubscribe = store.subscribe(()=> 
-            this.forceUpdate()
-        )
-    }
-    componentWillUnmount(){
-        this.unsubscribe();
-    }
-    render(){
-        const props = this.props;
-        const state = store.getState();
-
-        return ( 
-            <Link
-                active={
-                    props.filter === state.visibilityFilter
-                }
-                onClick={()=>
-                    store.dispatch({
-                    type: 'SET_VISIBILITY_FILTER', 
-                    filter: props.filter
-                    })
-                }
-            >
-            {props.children}
-            </Link>
-        );
-    }
-}
 const Link = ({
     active,
     children,
@@ -222,7 +216,7 @@ const Link = ({
         return <span>{children}</span>
     }
     return (
-        <a href='#'
+        <a href="https://google.com"
             onClick={ e => {
                 e.preventDefault();
                 onClick();
@@ -232,6 +226,57 @@ const Link = ({
         </a>
     );   
 }
+
+const mapStateToLinkProps = (state, ownProps) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
+    };
+}
+const mapDispatchToLinkProps = (dispatch, ownProps) =>{
+    return {
+        onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
+    };
+}
+
+//the connect function creates a new component
+const FilterLink = connect( 
+    mapStateToLinkProps,
+    mapDispatchToLinkProps
+)(Link);
+
+/*class FilterLink extends Component {
+
+    componentDidMount() {
+        const { store } = this.context;
+        this.unsubscribe = store.subscribe(()=> 
+            this.forceUpdate()
+        )
+    }
+    componentWillUnmount(){
+        this.unsubscribe();
+    }
+    render(){
+        const props = this.props;
+        const { store } = this.context;
+        const state = store.getState();
+
+        return ( 
+            <Link
+                active={
+                    props.filter === state.visibilityFilter
+                }
+                onClick={
+                }
+            >
+            {props.children}
+            </Link>
+        );
+    }
+}
+FilterLink.contextTypes = {
+    store: React.PropTypes.object
+}
+*/
 const getVisibleTodos = (
     todos,
     filter
@@ -252,7 +297,72 @@ const getVisibleTodos = (
     }
 
 }
-let nextTodoId = 0;
+
+/* the connect function gerates this component
+class VisibleTodoList extends Component {
+    componentDidMount() {
+        const { store } = this.context;
+        this.unsubscribe = store.subscribe(()=> 
+            this.forceUpdate()
+        )
+    }
+    componentWillUnmount(){
+        this.unsubscribe();
+    }
+
+    render(){
+        //const props = this.context;
+        const { store } = this.context;
+        const state = store.getState();
+        return (
+            <TodoList
+                todos
+                onTodoClick
+            />
+        );
+    }
+}
+VisibleTodoList.contextTypes = {
+    store: React.PropTypes.object
+}
+*/
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
+const mapStateTodoListToProps = (state) => {
+    return {
+        todos: getVisibleTodos(
+                        state.todos,
+                        state.visibilityFilter
+                    )
+    };
+}
+const mapDispatchTodoListToProps = (dispatch) => {
+    return {
+       onTodoClick: (id) => {
+                    dispatch(toggleTodo(id))
+       }
+    }
+}
+
+const  VisibleTodoList = connect(
+    mapStateTodoListToProps,
+    mapDispatchTodoListToProps
+)(TodoList);
+
 
 const Todo = ({
     onClick,
@@ -268,23 +378,10 @@ const Todo = ({
         {text}
     </li>
 );
-const TodoList = ({
-    todos,
-    onTodoClick
-}) => (
-    <ul>
-        {todos.map(todo => 
-            <Todo 
-                key={todo.id}
-                {...todo}
-                onClick={()=> onTodoClick(todo.id)}
-            />
-        )}
-    </ul>
-);
-const AddTodo = ({
-    onAddClick
-}) =>{
+
+let nextTodoId = 0;
+
+let AddTodo = ({ dispatch }) =>{
     let input;
     return (
         <div>
@@ -293,7 +390,7 @@ const AddTodo = ({
                 }} />
                 <button onClick={() => {
                     if(input.value.trim()!==""){
-                        onAddClick(input.value)
+                        dispatch(addTodo(input.value));
                         input.value='';
                     }
                 }}>
@@ -302,6 +399,11 @@ const AddTodo = ({
         </div>
     );
 }
+//connect call without any arguments
+//will generate a component.
+//it does not subscribe to the store
+//and it will inject the dispatch prop by default.
+AddTodo = connect( )(AddTodo);
 
 const Footer = () => (
     <p>
@@ -326,41 +428,16 @@ const Footer = () => (
         </FilterLink>
     </p> 
 );
-const TodoApp = ({
-    todos,
-    visibilityFilter
-}) =>
-    <div>
-    <AddTodo
-        onAddClick= { text => 
-            store.dispatch({
-                type:'ADD_TODO', 
-                id: nextTodoId++, 
-                text }) 
-        }
-    />
-    <TodoList
-        todos={
-            getVisibleTodos(
-                todos,
-                visibilityFilter
-            )
-        }
-        onTodoClick={id =>
-        store.dispatch({
-            type:'TOGGLE_TODO',
-            id
-        })}
-    />
+const TodoApp = () =>
+<div>
+    <AddTodo />
+    <VisibleTodoList />
     <Footer />
 </div>
 
 /*lesson 6*/
 //const store = createStore(counter);
 //lesson 14
-
-const store = createStore(oneAppForAllExamples);
-console.log(store.getState());
 /*
 store.dispatch({
     type:'ADD_TODO',
@@ -393,24 +470,38 @@ store.dispatch({
 
 console.log(store.getState().todo);
 */
-const render = () => {        
-    ReactDOM.render(
-        <div>
-            <Counter 
+/*    <Counter 
                 value = {store.getState().counter}
                 onIncrement = {() => store.dispatch({type:'INCREMENT'})}
                 onDecrement = {() => store.dispatch({type:'DECREMENT'})}
             />
-            <TodoApp 
-                {...store.getState()}
-            />
-        </div>, 
+          */
+/* removing this definition and using the one from react-redux
+class Provider extends Component {
+    getChildContext() {
+        return {
+            store: this.props.store
+        };
+    }
+    render(){
+        return this.props.children;
+    }
+}*/
+/*
+Provider.childContextTypes = {
+    store: React.PropTypes.object
+}
+*/
+const render = () => {        
+    ReactDOM.render(
+        <Provider store={createStore(oneAppForAllExamples)}>
+            <TodoApp />
+        </Provider>,
         document.getElementById('root')
     );
 }
 
-
-store.subscribe(render);
+//store.subscribe(render);
 render();
 
 
