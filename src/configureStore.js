025879f13f91a5f6c48f50a6c49a98b23011c2ad
Carlos Callirgos <callirgos01@gmaill.com>
2017-08-25 16:25:17 -0700

    removed local storage wrapped the store twise. once to add loggin, and one more time to make dispatch handle promises.

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,22 +1,41 @@
 
 import { createStore } from 'redux';
-import { loadState, saveState} from './localStorage';
-import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 
-const configureStore = () => {
-    const persistedState = loadState();
+const addLoggingToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+    if( !console.group){
+        return rawDispatch;
+    }
+    return (action) => {
+        console.group( action.type );
+        console.log('%c prev state', 'color: gray', store.getState());
+        console.log('%c action', 'color: blue', action);
+        const returnValue = rawDispatch(action);
+        console.log('%c next state', 'color: green',  store.getState());        
+        console.groupEnd(action.type);
+        return returnValue;
+    }
+}
+const addPromiseSupportToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+    return (action) => {
+        if( typeof action.then === 'function') {
+            return action.then(rawDispatch);
+        }
+        return rawDispatch(action);
+    }
+}
+const configureStore = () => {    
     const store = createStore(
-        todoApp,
-        persistedState
+        todoApp        
     )
-
-    store.subscribe(throttle(()=>{
-        saveState({
-            todos: store.getState().todos
-        });
-    },1000));
-    return store;
+    if( process.env.NODE_ENV !== 'production') {
+        store.dispatch = addLoggingToDispatch(store);
+    }    
+    
+    store.dispatch = addPromiseSupportToDispatch( store );
+    return store; 
 };
 
 export default configureStore;
