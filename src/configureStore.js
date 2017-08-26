479cbac344f511f183d3bbaf8c60a27a604a657c
Carlos Callirgos <callirgos01@gmaill.com>
2017-08-25 17:07:23 -0700

    hand rolled approach to middlware. great for understanding the concept of middleware. The first curried function in this tutorial.

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,39 +2,42 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
+const logger = (store) => (next) => {
     if( !console.group){
-        return rawDispatch;
+        return next;
     }
     return (action) => {
         console.group( action.type );
         console.log('%c prev state', 'color: gray', store.getState());
         console.log('%c action', 'color: blue', action);
-        const returnValue = rawDispatch(action);
+        const returnValue = next(action);
         console.log('%c next state', 'color: green',  store.getState());        
         console.groupEnd(action.type);
         return returnValue;
     }
 }
-const addPromiseSupportToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
-    return (action) => {
-        if( typeof action.then === 'function') {
-            return action.then(rawDispatch);
-        }
-        return rawDispatch(action);
+
+const promise = (store) => (next) => (action) => {
+    if( typeof action.then === 'function') {
+        return action.then(next);
     }
+    return next(action);
 }
 const configureStore = () => {    
-    const store = createStore(
-        todoApp        
-    )
+    const store = createStore( todoApp );
+    const middlewares = [promise];
+
     if( process.env.NODE_ENV !== 'production') {
-        store.dispatch = addLoggingToDispatch(store);
-    }    
-    
-    store.dispatch = addPromiseSupportToDispatch( store );
+        middlewares.push(logger);
+    }
+    const wrapDispatchWithMiddleWares = (store, middlewares) => {
+        middlewares.slice().reverse().forEach(middleware => 
+            store.dispatch = middleware(store)(store.dispatch)
+        );
+    }
+
+    wrapDispatchWithMiddleWares(store, middlewares);
+
     return store; 
 };
 
