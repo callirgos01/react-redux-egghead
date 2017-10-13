429aa8df7d66ebf85f7504af5327aace6fd6d56c
Callirgos, Carlos <carlos.callirgos@medtronic.com>
2017-10-13 12:16:26 -0700

    initial setup

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,10 +6,11 @@ const Todo = ({
     text
 }) => (
     <li onClick={onClick}
-        style = {{ 
-            textDecoration:
-                completed ? 'line-through' : 'none'
-        }}>
+        className={
+            completed ?
+                "todo-list__item--completed":
+                "todo-list__item--active"
+        }>
         {text}
     </li>
 );
